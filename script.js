877d09b4c84155643c4583f5f96c26648346faa7
clarinha-cell <00001121836069sp@al.educacao.sp.gov.br>
2025-06-19 22:31:05 -0300

    Update script.js
    Não tinha nada

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Você pode adicionar interações ou animações aqui depois :)
+// Você pode adicionar interações ou anima// Remove pré-carregamento depois de 2s
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.querySelector('.preload').style.display = 'none';
+  }, 2000);
+});
+
+// Rolar para seção ao clicar no botão
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}ções aqui depois :)
